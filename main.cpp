f412201fa5cb6bc51bc53e35d69f939a85cf97a5
NULL0404 <72699147+NULL0404@users.noreply.github.com>
2021-11-18 17:10:29 -0700

    pneumatics
    untested and github deleted my last commit so this is gonan be low effort

--- main.cpp
+++ main.cpp
@@ -24,16 +24,35 @@ It's a private repository, you will need to log in and or have me add you to the
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button() {
+void on_left_button() {
+	
+	#define DIGITAL_SENSOR_PORT 'A'
+	
+	
+	//pros::ADIDigitalOut piston (uint8_t A);
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+		//piston.set_value(true);
+		pros::ADIDigitalOut piston (DIGITAL_SENSOR_PORT);
+  		piston.set_value(true);
+  		pros::delay(1000);
+  		piston.set_value(false);
 	} else {
 		pros::lcd::clear_line(2);
 	}
 }
 
+void on_right_button() {
+//	pros::ADIDigitalOut piston (uint8_t A);
+//	static bool pressed = false;
+//	pressed = !pressed;
+//	if (pressed) {
+//		piston.set_value(false);
+//	} else {
+//		pros::lcd::clear_line(2);
+//	}
+}
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -45,8 +64,9 @@ void initialize() {
 	pros::lcd::set_text(1, "hello gamer pro gamer");
 	pros::lcd::set_text(2, "linux is cool");
 
-	pros::lcd::register_btn1_cb(on_center_button);
-	
+	pros::lcd::register_btn1_cb(on_left_button);
+	pros::lcd::register_btn1_cb(on_right_button);
+
 
 
 }
@@ -113,6 +133,10 @@ void opcontrol() {
 
 	pros::Motor ghgh(7); 
 	
+	
+	
+	
+	
 	while (true) {
 //		pros::c::battery_get_capacity batcapacity;
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2, // Left button
@@ -123,18 +147,38 @@ void opcontrol() {
 		int left =- master.get_analog(ANALOG_LEFT_Y);					// Controller tank controls
 		int right = master.get_analog(ANALOG_RIGHT_Y);					// Goes to variables 'left' and 'right'
 		
-		int lift1 = master.get_digital(DIGITAL_L1) - master.get_digital(DIGITAL_L2);
+		int lift1 = master.get_digital(DIGITAL_L1) - master.get_digital(DIGITAL_L2);	// Lift
 
+		int miving = master.get_digital(DIGITAL_R1) - master.get_digital(DIGITAL_R2);
 
-		left_mtr1 = left;								// Sets motor speed for 'left'
-		left_mtr2 = left;								//
+		
+		// Speed multiplier 
+/*		if (master.get_digital(DIGITAL_L2 = 0) {
+			if (master.get_digital(DIGITAL_L1 = 0) {
+				int linux = 0.7;
+			}
 			
-		right_mtr5 = right;								// Sets motor speed for 'right'
-		right_mtr4 = right;
+			else {
+				if (master.get_digital(DIGITAL_L2 = 0) {
+					int linux = 0.3
+				}
+				else {
+					int linux = 1
+				}
+			}	
+		} 
+*/
+		left_mtr1 = left;// * linux;								// Sets motor speed for 'left'
+		left_mtr2 = left;// * linux;								//
+			
+		right_mtr5 = right;// * linux;								// Sets motor speed for 'right'
+		right_mtr4 = right;// * linux;
 		
 		ghgh = lift1;
-
+		
 		// Reverse the above motors by putting a - after the =
+		
+		
 		
 		pros::delay(20);								// Delay 
 		
